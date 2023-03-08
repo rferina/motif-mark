c85742cc9c54a0821d5b07addfb56918300c7801
rferina <rferina@uoregon.edu>
2023-03-08 15:45:45 -0800

    updated README, commented code

--- README.md
+++ README.md
@@ -3,7 +3,16 @@
 
 ## Overview
 
-This script takes in an input fasta file and input motif file. It outputs an image (png file) with the same name as the input fasta file.
+This script takes in an input FASTA file and input motif file. It outputs an image (png file) with the same name as the input fasta file.
+
+Among other elements, a gene includes exons, which are regions that code for proteins, and introns, which are non-protein coding regions. Typically, introns are spliced out to form mature mRNA which can then be translated into protein. Sometimes exons are also spliced out, known as 
+casette exons. Casette exons can be alternatively spliced, remaining either in or out of the gene depending on developmental stage and the environment. Motifs are small repeating patterns of sequences that can serve as binding sites of regulatory proteins. When regulatory proteins bind to motifs, it can activate or suppress the inclusion of exons.
+
+This script assumes the input FASTA file contains the sequence of the casette exon, and parts of the surrounding introns. It also assumes the 
+sequences are all in the same orientation (reverse complement is not accounted for).
+
+The gene is mapped in the output figure, along with the casette exon. Which motifs are present in all the genes are shown in different colors.
+This figure is drawn to scale. Note that 100 was added to every position so the drawings weren't off the page.
 
 
 ## Software Versions
@@ -15,11 +24,14 @@ The conda environment my_pycairo must be activated, which includes Pycairo versi
 ## Command to Run Script
 
 The conda environment must first be activated.
-    conda activate my_pycairo
+    
+    **conda activate my_pycairo**
 
 To run the script, the following argparse arguments must be specified.
--f: the input fasta filename
--m: the input motif filename
+
+    **-f: the input fasta filename**
+    **-m: the input motif filename**
 
 Full example:
-    python motif-mark-oop.py -f test_fa.fasta -m test_motif.txt
+
+    **python motif-mark-oop.py -f test_fa.fasta -m test_motif.txt**
--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -14,6 +14,7 @@ def get_args():
 
 # conda activate my_pycairo
 # command to run: python motif-mark-oop.py -f test_fa.fasta -m test_motif.txt
+# python motif-mark-oop.py -f Figure_1.fasta -m Fig_1_motifs.txt
 
 args = get_args()
 
@@ -29,7 +30,7 @@ oneline_file = bioinfo.oneline_fasta(fasta_file)
 print('one_line complete')
 
 def create_context(width, height):
-    '''Takes in desired width and height, returns the context for drawing.'''
+    '''Takes in desired width and height, returns the surface and context for pycairo drawing.'''
     # create the coordinates to display graphic, desginate output
     surface = cairo.PDFSurface("output.pdf", width, height)
     # create the coordinates to draw on
@@ -37,7 +38,7 @@ def create_context(width, height):
     return surface, context
 
 # create context to draw on
-surface, context = create_context(1000, 1200)
+surface, context = create_context(1200, 1200)
 
 # make background white
 context.save()
@@ -50,9 +51,10 @@ context.set_font_size(15)
 context.select_font_face("Arial",
             cairo.FONT_SLANT_NORMAL,
             cairo.FONT_WEIGHT_NORMAL)
-context.move_to(400, 20)
+context.move_to(500, 20)
 context.show_text('Motif Marker Visualizer')
 
+# make legend
 colors = [[0, 1, 1, 0.5], [0.75, 0.24, 1, 0.5], [0.2, 0.63, 0.79, 0.5],
            [0.41, 0.55, 0.13, 0.5], [1, 0.27, 0, 0.5], [0, 0, 1, 0.5]]
 
@@ -61,20 +63,19 @@ y_list = [50, 75, 100, 125, 150, 175]
 for color, y in zip(colors, y_list):
     context.set_line_width(25)
     context.set_source_rgba(color[0], color[1], color[2], color[3])
-    context.move_to(800, y)        #(x,y)
-    context.line_to(810, y)
+    context.move_to(1115, y)        #(x,y)
+    context.line_to(1125, y)
     context.stroke()
 
 
-
-# context.rectangle(0, 0, 800, 1000)
-# context.set_source_rgb(1, 1, 1)
-# context.fill()
-
-
-
 class Gene:
-    def __init__(self, oneline_fasta,  gene_start, gene_stop, gene_name, gene_number) -> None:  # gene_start, gene_stop, gene_name
+    '''This class represents a gene's sequence from a FASTA file. It has a method to draw the
+    gene to scale via Pycairo, and the gene name is also recorded on the drawing.
+    '''
+    def __init__(self, oneline_fasta, gene_start, gene_stop, gene_name, gene_number) -> None:  
+        '''Takes in a fasta file where each sequence is on one line. Also takes in and intializes the index
+        of where the gene starts and stops, as well as the gene name, and gene number (gene number
+        corresponds to a sequence line in the fasta file.)'''
         self.oneline_fa = oneline_fasta
         self.gene_start = gene_start
         self.gene_stop = gene_stop
@@ -82,10 +83,11 @@ class Gene:
         self.gene_number = gene_number
     
     def draw_gene(self):
-        # draw gene
+        '''Returns the gene drawing with gene name.'''
+        # set line width, black color
         context.set_line_width(2)
         context.set_source_rgba(0, 0, 0)
-
+        # draw first gene from fasta file
         if self.gene_number == 1:
             context.move_to(self.gene_start, 75)        #(x,y)
             context.line_to(self.gene_stop, 75)
@@ -97,6 +99,8 @@ class Gene:
                         cairo.FONT_WEIGHT_NORMAL)
             context.move_to(50, 50)
             context.show_text(self.gene_name)
+        # draw subsequent genes from fasta file, adjusting for the y value
+        # of where they'll be drawn
         elif self.gene_number > 1:
             y_val = self.gene_number * 75
             context.move_to(self.gene_start, y_val)        #(x,y)
@@ -111,54 +115,60 @@ class Gene:
             context.show_text(self.gene_name)
 
 
-class Exon(Gene):
+class Exon:
+    '''This class represents the casette exon present in the FASTA file sequence. It has a method
+    to draw the exon to scale via Pycairo.
+    '''
     def __init__(self, exon_start, exon_stop):
-        # super().__init_subclass__ (exon_start, exon_stop):
-        """
-        Takes in a file path and initializes an
-        inverted index (a dictionary) with keys as the
-        term and values as the list of documents with
-        that term.
-        """
-        # self.path = path
+        """Takes in and initializes the exon start and stop positions."""
         self.exon_start = exon_start
         self.exon_stop = exon_stop
 
     def draw_exon(self):
+        '''Returns the exon drawing.'''
+        # set line width, black color
+        context.set_line_width(10)
+        context.set_source_rgba(0, 0, 0)
+        # draw exon associated with first gene from fasta file
         if gene_1.gene_number == 1:
-            context.set_line_width(10)
-            context.set_source_rgba(0, 0, 0)
-            context.move_to(self.exon_start,75)        #(x,y)
-            context.line_to(self.exon_stop,75)
+            context.move_to(self.exon_start, 75)        #(x,y)
+            context.line_to(self.exon_stop, 75)
             context.stroke()
+        # draw exons associated with subsequent genes from fasta file, adjusting for the y value
+        # of where they'll be drawn
         elif gene_1.gene_number > 1:
-            context.set_line_width(10)
-            context.set_source_rgba(0, 0, 0)
             y_val = gene_1.gene_number * 75
             context.move_to(self.exon_start, y_val)        #(x,y)
             context.line_to(self.exon_stop, y_val)
             context.stroke()
 
 
-
 class Motif:
+    '''This class represents the motifs that can be found in a gene's sequence in a FASTA file. It has a method to draw
+    the motifs to scale via Pycairo, with unique colors for each motif.'''
     def __init__(self, motif_start, motif_stop) -> None:
+        '''Takes in and initializes the motif start and stop positions.'''
         self.motif_start = motif_start
         self.motif_stop = motif_stop
 
     def draw_motifs(self):
+        '''Returns the motifs drawing.'''
+        motif_count = 0
         if gene_1.gene_number == 1:
+            motif_count += 1
             context.set_line_width(25)
-            context.set_source_rgba(4, 0, 4, 0.5)
-            # context.set_source_rgba(colors)
-            context.move_to(self.motif_start,75)        #(x,y)
-            context.line_to(self.motif_stop,75)
+            # context.set_source_rgba(4, 0, 4, 0.5)
+            context.set_source_rgba(colors[motif_count][0], colors[motif_count][1], colors[motif_count][2], colors[motif_count][3])
+
+            context.move_to(self.motif_start, 75)        #(x,y)
+            context.line_to(self.motif_stop, 75)
             context.stroke()
         elif gene_1.gene_number > 1:
+            motif_count += 1
             y_val = gene_1.gene_number * 75
             context.set_line_width(25)
-            context.set_source_rgba(4, 0, 4, 0.5)
-            # context.set_source_rgba(colors)
+            # context.set_source_rgba(4, 0, 4, 0.5)
+            context.set_source_rgba(colors[motif_count][0], colors[motif_count][1], colors[motif_count][2], colors[motif_count][3])
             context.move_to(self.motif_start, y_val)        #(x,y)
             context.line_to(self.motif_stop, y_val)
             context.stroke()
@@ -168,25 +178,10 @@ class Motif:
 
 # count to add one everytime thru seq, start -1; index motif_count
 
+# need color to be associated with motif, not gene
 
 
-########### RUN STUFF #####################################################################
-# mygene = Gene(600, 'gene ayooo')
-# mygene.identify()
-# mygene.draw_gene()
-
-# mygene = Gene('fa_one_line.fa')
-# # mygene = Gene(oneline_file, 100, 600, 'gene1 test')
-# mygene.draw_gene(100, 600, 'gene2 test')
-
-# myexon = Exon(200, 300)
-# myexon.draw_exon()
-
-# mymotif = Motif(100, 105)
-# mymotif.draw_motifs()
-
-
-# IUPAC dictionary
+# IUPAC dictionary for ambiguous motifs
 iupac_dict = {
     "A": "[Aa]",
     "C": "[Cc]",
@@ -207,32 +202,29 @@ iupac_dict = {
     "Z": "[]"
 }
 
-# .replace with all
-
-# open motif file
+# open motif file, generate list of possible motifs
 motifs = open(motifs_file, 'r')
 line_number = 0
 motifs_list = []
 for entry in motifs:
     line_number += 1
     entry = entry.strip('\n')
-    # translate motif entries if needed
+    # translate ambiguous motif entries if needed
     motif_option = ''
     for nuc in entry:
         if nuc in iupac_dict.keys():
             motif_option += iupac_dict[nuc]
-        # no conversion
+        # if translation not needed
         else:
-            motif_option += nuc
-        
+            motif_option += nuc   
     motifs_list.append(motif_option) 
 print('motifs list', motifs_list)
 
-# with open, generate Gene obj for each line
-testing_file = open('fa_one_line.fa', 'r')
+# open one line fasta file, generate Gene object for each sequence line
+fasta_one_line_file = open('fa_one_line.fa', 'r')
 line_count = 0
 gene_num = 0
-for line in testing_file:
+for line in fasta_one_line_file:
         line_count +=1
         line = line.strip('\n')
         # define gene name as header line
@@ -244,22 +236,22 @@ for line in testing_file:
             gene_beg = 100 
             gene_end = len(line) + gene_beg
             gene_num += 1
-            # make gene object
-            gene_1 = Gene(testing_file, gene_beg, gene_end, gene_id, gene_num)
+            # make gene object for each sequence line, draw genes
+            gene_1 = Gene(fasta_one_line_file, gene_beg, gene_end, gene_id, gene_num)
             print('made obj')
             gene_1.draw_gene()
             print('drew')
             print(gene_1.gene_number)
-            # define exon terms
+            # identify start and stop positions of exons
             exon_indexes = []
             for match in re.finditer(r'[A-Z]', line):
                 exon_indexes.append(match.start())
                 ex_st = exon_indexes[0] + 100
                 ex_en = exon_indexes[-1] + 100
+                # generate Exon objects and draw them
                 myexon = Exon(ex_st, ex_en)
                 myexon.draw_exon()
-            print(ex_st, ex_en)
-            # look for motifs
+            # look for motifs in the sequence lines
             for motif in motifs_list:
                 motif_indexes = {}
                 motif_list_ind = [] 
@@ -271,202 +263,32 @@ for line in testing_file:
                     if motif not in motif_indexes:
                         motif_indexes[motif] = motif_list_ind
                 print(motif_indexes)
-                # for motifs on each gene, draw all
+                # for motifs on each gene, draw all the motifs
                 for keys in motif_indexes.keys():
+                    # if there's only one instance of the motif
                     if len(motif_indexes[keys]) == 1:
                         motif_count = len(motif_indexes[keys])
                         motif_start = motif_indexes[keys][0][0]
                         motif_stop = motif_indexes[keys][0][1]
+                        # generate motif object, draw motif
                         mymotif = Motif(motif_start, motif_stop)
                         mymotif.draw_motifs()
+                    # if there's multiple instances of the motif
                     elif len(motif_indexes[keys]) > 1:
                         motif_count = len(motif_indexes[keys])
                         # iterate through the list of tuples in the motif dictionary values
                         for i in range(len(motif_indexes[keys])):
                             motif_start_i = motif_indexes[keys][i][0]
                             motif_stop_i = motif_indexes[keys][i][1]
+                            # generate motif objects, draw the multiple motifs
                             mymotif2 = Motif(motif_start_i, motif_stop_i)
                             mymotif2.draw_motifs()
         
-                
-      
-
 
 # complete drawing
 surface.write_to_png (png_name)
 surface.finish()
 
-testing_file.close()
+# close files
+fasta_one_line_file.close()
 motifs.close()
-######################################################################################
-
-##############################################################################################
-
-    # dict with key as captialize, value is set of what it represents; if key in motif look in set
-    # {Y:, (C, T)}
-    # change y to t or c
-    # have regex look for every possibility
-    # re.replace()
-    # result = re.sub('abc',  'def', input)
-#   will have c|t then regex will use that to find it
-
-
-
-# notes
-
-# could have diff num of motifs, exons, stick inside list dict or another object
-# "gene" 2 
-# class FilingCabinet: # dict or obj
-#     def __init__(self) -> None:
-#         pass
-#     def 
-
-# gene1 is a object of type line
-# exon1 is object of type line
-# fc1=FilingCabinet(gene1, exon1,motifs1) # use * args or a motifs list of line objects motif1a, motif1b; drawn at 300
-# fc2=FilingCabinet(gene2, exon2, motifs2, ) # motifs 2 list; drawn at 600
-# if do this, in MAIN fcl1.draw(context) fcl2.draw(context) (this is filing cabinet draw, not line draw)
-
-# dif color for each motif type
-
-################## TO DO########################################################################################
-# in MAIN 
-# parse args
-# define classes, functions, constants
-# create context
-# clean FASTA
-# translate ambiguous motifs; make it usable regex; in data structure
-# parse clean FASTA file; look for gene (length of seq, store in line obj or filing cabinet), exon (uppercase to identify start/stop stored as line obj),
-#        motifs (line obj); need each to belong to a group; not just list; gene names =?? in dict
-# draw by looking thru all filing cabinets (collection of filing cabinets in a list)
-    # for fc in filing_cabinets:
-    #     fc.draw(context)
-
-# for motif1a, motif1b, motif1c
-# motifs = []
-# for spec in specs: #specifications 13,19,'motif'
-#     motif= Line(*spec) # explode an array
-#     # store motif obj in list
-#     motifs.append(motif)
-
-# len(motifs) should be 3 for this [Line(...), Line(..., Line(...))]
-# list of lists
-
-# dict with key
-# fasta header can be gene name
-
-# make test input file
-# aaaaaAATTAAaaaaa
-# motif YY could be TT or CC
-# search for all motifs in each gene
-# can use random colors for diff motifs; or overlapping motifs stack them (transparent colors?)
-# give additional info for second overlapping one, or motifs class to collect all, motifs1.draw can see overlapping
-
-# easy bonus, implement it, make copy of it, change code to implement inheritance just rearranging
-
-
-# could remove y parameter, instead put in draw method
-
-
-# # with open(args.fasta, 'r')
-
-
-# class Shape()
-#     pass
-# class Circle<Shape: # circle inherits from shape
-#     def draw
-# class Rectangle<Shape:
-#     def draw
-
-# # draw circles in pycairo class
-
-################### old MAIN ###################################################################
-# input = 'ygcy'    
-# output = input.replace("y", "[c|t]")
-# print(output)
-
-# # detect change in case for start of exon
-test='aaaBDBcc' # 3-5
-# res = re.search(r'\B[A-Z]\B', test) # end not right
-# print(res.end()) # start is pos with 0 indexing
-
-# res = [idx for idx in range(len(test)) if test[idx].isupper()]
-# print(res)
-
-# res2 = [match.start() for match in re.finditer(r'[A-Z]', test)]
-# print(res2[0], res2[-1])
-# res3 = []
-# for match in re.finditer(r'[A-Z]', test):
-#     res3.append(match.start())
-#     ex_st = res3[0]
-#     ex_en = res3[-1]
-# print(ex_st, ex_en)
-# print(res3)
-
-# see if case == case at start pos to get end pos?
-# res2 = re.finditer(r'\B[A-Z]\B', test) # end not right
-# for match in re.finditer(r'\B[A-Z]\B', test):
-# #     # print('end', match.end())
-#     print("start index", match.start(), "End index", match.end())
-    # print(match.group(), "start index", match.start(), "End index", match.end()) # but end of exon is -1
-
-
-# Position.oneline_fa('Figure_1.fasta')
-
-# # Position.oneline_fa('Figure_1.fasta', 'Figure_1.fasta')
-
-# Exon.identify_exon('fa_one_line.fa')
-
-####################################################################################################
-
-
-# make exon object from data; could name, throw in dict or list
-
-
-# class people, each one is a noun; all have a draw functionality
-# context obj like piece of paper; white male draws differently
-# justine = Person(no hat, brown eyes)
-# justine.draw(context object)
-# object.draw(context)
-##########################################
-# if __name__== "__main__":
-    # input = 'ygcy'    
-    # output = input.replace("y", "[c|t]")
-    # print(output)
-
-#     # detect change in case for start of exon
-#     test='aaaBDBcc'
-#     res = re.search(r'\B[A-Z]\B', test) # end not right
-#     print(res.start())
-#     # see if case == case at start pos to get end pos?
-#     # res2 = re.finditer(r'\B[A-Z]\B', test) # end not right
-#     for match in re.finditer(r'\B[A-Z]\B', test):
-#         # print('end', match.end())
-#         print(match.group(), "start index", match.start(), "End index", match.end()) # but end of exon is -1
-
-
-#     Position.oneline_fa('Figure_1.fasta')
-#     # Position.oneline_fa('Figure_1.fasta', 'Figure_1.fasta')
-
-#     Exon.identify_exon('fa_one_line.fa')
-
-    # file1.oneline_fa()
-
-    # result = re.sub('y', 't', input) 
-    # result2 = re.sub('y', 'c', input)
-
-# seq = 'aaaaaaaaCACGGTTGGCACaaaaa'
-# mot_test = ['CAC', 'TT']
-# for m in mot_test:
-#     mot_indexes = {}
-#     mot_list = []
-#     for match in re.finditer(m, seq):
-#         mot_list.append((match.start()+100, match.end()+100))
-#         if m not in mot_indexes:
-#             mot_indexes[m] = mot_list
-#     print(mot_indexes)
-
-
-# newObject = originalString.replace('character to replace', 'character to be replaced with, count of replacements to perform)
-
-# append all instances to list, then put list as dict val
